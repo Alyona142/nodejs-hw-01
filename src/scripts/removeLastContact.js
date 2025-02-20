ddb6c0ebe541cd50723cd93170272fa4168bd1ab
Alyona <olenapav97@gmail.com>
2025-02-20 20:18:52 +0200

    qwa

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,16 +1,22 @@
 import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
-const removeLastContacts = () => {
+const removeLastContact = () => {
   try {
     const contacts = readContacts();
+
+    if (!Array.isArray(contacts)) {
+      throw new Error('The data from the file is not an array!');
+    }
+
     if (contacts.length === 0) {
       console.log('There are no contacts to delete.');
       return;
     }
 
-    contacts.pop();
-    writeContacts(contacts);
+    const updatedContacts = contacts.slice(0, contacts.length - 1);
+    writeContacts(updatedContacts);
+
     console.log('The last contact has been deleted.');
   } catch (error) {
     console.error(
@@ -20,4 +26,4 @@ const removeLastContacts = () => {
   }
 };
 
-removeLastContacts();
+removeLastContact();
